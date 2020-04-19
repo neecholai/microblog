68538ef7c4a2fb9f4a75e371f2785522b66cdead
Nicholai Hansen <nicholaihansen1993@gmail.com>
2020-04-19 14:34:14 -0700

    styled home page cards

--- frontend/src/post/BlogPostCard.js
+++ frontend/src/post/BlogPostCard.js
@@ -3,28 +3,30 @@ import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { voteForPost } from '../redux/actions';
 import './BlogPostCard.css';
-
-
-
+import { Card, Button } from 'react-bootstrap';
 
 function BlogPostCard({ id, title, description, votes }) {
   const dispatch = useDispatch();
 
   const handleVote = (direction) => {
     dispatch(voteForPost(id, direction));
-  }
+  };
 
   return (
-    <div className="BlogPostCard">
-      <Link to={`/posts/${id}`} className="BLogPostCard-title">{title}</Link>
-      <span className="BlogPostCard-description">{description}</span>
-      <div className="BlogPostCard-voteWrapper">
-        <span className="BlogPostCard-votes">{votes}</span>
-        <i onClick={() => handleVote("up")} className="fas fa-thumbs-up"></i>
-        <i onClick={() => handleVote("down")} className="fas fa-thumbs-down"></i>
-      </div>
-    </div>
-  )
+    <Card border="secondary" as={Link} to={`/posts/${id}`} className="m-3 BlogPostCard col-xs-8 col-sm-6 col-md-4">
+      <Card.Body className="BlogPostCard-body">
+        <Card.Title className="BlogPostCard-title">
+          {title}
+        </Card.Title>
+        <Card.Text className="BlogPostCard-description">{description}</Card.Text>
+        <Card.Text className="BlogPostCard-voteWrapper">
+          <span className="px-4 BlogPostCard-votes">{votes}</span>
+          <i onClick={() => handleVote('up')} className="px-1 fas fa-thumbs-up"></i>
+          <i onClick={() => handleVote('down')} className="px-1 fas fa-thumbs-down"></i>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default BlogPostCard;
